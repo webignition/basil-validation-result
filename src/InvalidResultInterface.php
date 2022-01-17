be3696a2146d976238d6ef3e583b2428b81c9fcb
Jon Cram <webignition@gmail.com>
2022-01-17 15:04:21 +0000

    Use php-cs-fixer, use phpstan level 9 (#12)

--- src/InvalidResultInterface.php
+++ src/InvalidResultInterface.php
@@ -7,7 +7,9 @@ namespace webignition\BasilValidationResult;
 interface InvalidResultInterface extends ResultInterface
 {
     public function getType(): string;
+
     public function getReason(): string;
+
     public function getPrevious(): ?InvalidResultInterface;
 
     /**
